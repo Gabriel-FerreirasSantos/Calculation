f39b2dc52279d15e646fa8089f8350cc7005da36
Gabriel-FerreirasSantos <gabriel.ruasmil@gmail.com>
2022-10-27 20:51:56 -0300

    update

--- calculadora/calculadora_main.py
+++ calculadora/calculadora_main.py
@@ -4,33 +4,33 @@ def contademais():
     a = int(input('Digite um numero: '))
     b = int(input('Digite outro: '))
     s = (a + b)
-    print(s)
+    print('Resultado =',s)
 
 def contademenos():
     a = int(input('Digite um numero: '))
     b = int(input('Digite outro: '))
     s = (a - b)
-    print(s)    
+    print('Resultado =',s)    
 
 def contadedividir():
     a = float(input('Digite um numero: '))
     b = float(input('Digite outro: '))
     s3 = (a / b)
-    print(s3)
+    print('Resultado =',s3)
 
 def contademutiplicação():
     a = int(input('Digite um numero: '))
     b = int(input('Digite outro: '))
     s4 = (a * b)
-    print(s4)  
+    print('Resultado =',s4)  
 
 def contaderaiz():
     num = float(input("Digite um numero:\n"))
     raiz = math.pow(num, 1/2)
-    print('{:.1f}'.format(raiz))
+    print('Resultado =','{:.1f}'.format(raiz))
 
 def contadepotenciação():
     a = float(input('Digite um numero: '))
     b = float(input('Digite outro: '))
     s6 = (a ** b)
-    print('{:.1f}'.format(s6))                     
+    print('Resultado =','{:.1f}'.format(s6))                     
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #import's
+import string
 from calculadora import calculadora_main
 
+from replit import clear
 
 #Programa principal
 Variavel_while1 = False
 
+
 while Variavel_while1 == False:
+    clear()
     print('-=' * 25)
     print(' ' * 10, 'Bem vindo ao painel da Takern')
     print('-=' * 25)
@@ -14,75 +18,115 @@ while Variavel_while1 == False:
     print('[2] Fazer exercicios')
     print('[3] Fechar o programa')
 
+    
     escolha = str(input(''))
-    escolha = escolha.replace('', ' ').strip()
+    escolha = escolha.strip()
 
 
     #verificar se o numero não é numerico
-    if escolha.isalpha():
+    if not escolha.isnumeric():
         print('Você não digitou um número')
         print('Você precisa digitar um número!')
-        print('Esse numero não existe deseja voltar? (s/n)')
-        voltar = input('').upper()
-        voltar = voltar.replace('', ' ') 
-        if voltar == 'S':
-            continue
-        elif voltar == 'N':
-            print('Volte sempre!')
-            break
+        break
         
         
         
     escolha = int(escolha) #Transformo a escolha que esta em string para numero inteiro
 
-    
-    if escolha == 3:
-        break
 
     #verificação de qual numero foi escolhido
     while True:
         if escolha == 1:
-            print('[1]Adição')
-            print('[2]Subtração')
-            print('[3]Divisão')
-            print('[4]Mutiplicação')
-            print('[5]Raiz quadrada')
-            print('[6]Potenciação')
-            escolha2 = input('')
-            if escolha2 == '1':
-                calculadora_main.contademais()
+            while True:
+                clear()
+                print('-=' * 25)
+                print(' ' * 10, 'Bem vindo a calculadora')
+                print('-=' * 25)
+                print('[1]Adição')
+                print('[2]Subtração')
+                print('[3]Divisão')
+                print('[4]Mutiplicação')
+                print('[5]Raiz quadrada')
+                print('[6]Potenciação')
+                print('[7]Voltar')
+                escolha2 = input('')
+                if escolha2 == '1':
+                    while True:
+                        clear()
+                        calculadora_main.contademais()
+                        print('Aperte qualquer botão para voltar')
+                        botao = input('')
+                        break
 
-            elif escolha2 == '2':
-                calculadora_main.contademenos()
+                elif escolha2 == '2':
+                    while True:
+                        clear()
+                        calculadora_main.contademenos()
+                        print('Aperte qualquer botão para voltar')
+                        botao = input('')
+                        break
 
-            elif escolha2 == '3':
-                calculadora_main.contadedividir()
+                elif escolha2 == '3':
+                    while True:
+                        clear()
+                        calculadora_main.contadedividir()
+                        print('Aperte qualquer botão para voltar')
+                        botao = input('')
+                        break
 
-            elif escolha2 == '4':
-                calculadora_main.contademutiplicação() 
+                elif escolha2 == '4':
+                    while True:
+                        clear()
+                        calculadora_main.contademutiplicação() 
+                        print('Aperte qualquer botão para voltar')
+                        botao = input('')
+                        break
 
-            elif escolha2 == '5':
-                calculadora_main.contaderaiz()
-                
-            elif escolha2 == '6':
-                calculadora_main.contadepotenciação() 
+                elif escolha2 == '5':
+                    while True:
+                        clear()
+                        calculadora_main.contaderaiz()
+                        print('Aperte qualquer botão para voltar')
+                        botao = input('')
+                        break
+
+                elif escolha2 == '6':
+                    while True:
+                        clear()
+                        calculadora_main.contadepotenciação() 
+                        print('Aperte qualquer botão para voltar')
+                        botao = input('')
+                        break
+                    
+                elif escolha2 == '7':
+                    break
+
+                else:
+                    print('caiu no break')
+                    break
 
         elif escolha == 2:
             pass
+
+        if escolha == 3:
+            Variavel_while1 = True
+            break
 
         else:
             print('Você não digitou um número da lista')
             print('Você precisa digitar um número que esta na lista!')
             print('Deseja voltar? (s/n)')
-            voltar = str(input('')).strip().lower().replace('', ' ') 
+            voltar = str(input('')).strip().lower()
             if voltar == 's':
-                pass
+                break
             elif voltar == 'n':
                 print('Volte sempre!')
                 Variavel_while1 = True
+                break
             else:
                 print('Escreva direito!')
                 Variavel_while1 = True
+                break
 
         
         

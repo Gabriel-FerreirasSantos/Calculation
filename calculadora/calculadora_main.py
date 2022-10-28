49d44c7cd6d06e3e4ffdf41a70bc3898453d5656
Gabriel-FerreirasSantos <gabriel.ruasmil@gmail.com>
2022-10-28 20:03:14 -0300

    update

--- calculadora/calculadora_main.py
+++ calculadora/calculadora_main.py
@@ -4,39 +4,69 @@ def contademais():
     while True:
         a = input('Digite um numero: ')
         b = input('Digite outro: ')
-        if not a.isnumeric() and not b.isnumeric():
+        if not a.isnumeric() or not b.isnumeric():
             print('Você não digitou um número')
             print('Você precisa digitar um número!')
             break
-        s = int(a) + int(b)
+        s = float(a) + float(b)
         print('Resultado = {:.2f}'.format(s))
         break
 
 def contademenos():
-    a = int(input('Digite um numero: '))
-    b = int(input('Digite outro: '))
-    s = (a - b)
-    print('Resultado = {:.2f}'.format(s))    
+    while True:
+        a = input('Digite um numero: ')
+        b = input('Digite outro: ')
+        if not a.isnumeric() or not b.isnumeric():
+            print('Você não digitou um número')
+            print('Você precisa digitar um número!')
+            break
+        s = float(a) - float(b)
+        print('Resultado = {:.2f}'.format(s))
+        break   
 
 def contadedividir():
-    a = float(input('Digite um numero: '))
-    b = float(input('Digite outro: '))
-    s3 = (a / b)
-    print('Resultado =',s3)
+    while True:
+        a = input('Digite um numero: ')
+        b = input('Digite outro: ')
+        if not a.isnumeric() or not b.isnumeric():
+            print('Você não digitou um número')
+            print('Você precisa digitar um número!')
+            break
+        s3 = float(a) /  float(b)
+        print('Resultado = {:.2f}'.format(s3))
+        break
 
 def contademutiplicação():
-    a = int(input('Digite um numero: '))
-    b = int(input('Digite outro: '))
-    s4 = (a * b)
-    print('Resultado = {:.2f}'.format(s4))  
+    while True:
+        a = input('Digite um numero: ')
+        b = input('Digite outro: ')
+        if not a.isnumeric() or not b.isnumeric():
+            print('Você não digitou um número')
+            print('Você precisa digitar um número!')
+            break
+        s4 = float(a) * float(b)
+        print('Resultado = {:.2f}'.format(s4))  
+        break
 
 def contaderaiz():
-    num = float(input("Digite um numero:\n"))
-    raiz = math.pow(num, 1/2)
-    print('Resultado =','{:.2f}'.format(raiz))
+    while True:
+        num = input("Digite um numero:\n")
+        if not num.isnumeric():
+            print('Você não digitou um número')
+            print('Você precisa digitar um número!')
+            break
+        raiz = math.pow(float(num), 1/2)
+        print('Resultado =','{:.2f}'.format(raiz))
+        break
 
 def contadepotenciação():
-    a = float(input('Digite um numero: '))
-    b = float(input('Digite outro: '))
-    s6 = (a ** b)
-    print('Resultado =','{:.2f}'.format(s6))                     
+    while True:
+        a = input('Digite um numero: ')
+        b = input('Digite outro: ')
+        if not a.isnumeric() or not b.isnumeric():
+            print('Você não digitou um número')
+            print('Você precisa digitar um número!')
+            break
+        s6 = float(a) ** float(b)
+        print('Resultado =','{:.2f}'.format(s6))     
+        break                

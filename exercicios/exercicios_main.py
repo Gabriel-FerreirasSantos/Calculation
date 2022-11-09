bfb64c6344bf3bdaa837f3cc786482ac06e8030f
Gabriel-FerreirasSantos <gabriel.ruasmil@gmail.com>
2022-11-08 22:30:06 -0300

    update

--- exercicios/exercicios_main.py
+++ exercicios/exercicios_main.py
@@ -3,14 +3,24 @@ import random
 
 
 def exercicio_adiçãoleve1():
+    clear()
+    print('Resolva essa conta:')
     x = random.randint(0, 100)
     y = random.randint(0, 100)
-    resultado = x + y
-    resposta = input(f'{x} + {y} =')
+    resultado = int(x) + int(y)
+    resposta = input(f'{x} + {y} =').replace('', ' ')
+    while True:
+        if not resposta.isnumeric():
+            print('Você não digitou um número')
+            print('Você precisa digitar um número!')
+            break
+
+    resposta = int(resposta)
     while True:
         if resposta == resultado:
             print('Acertou!')
             print(f'{x} + {y} = {resultado}')
+            
         elif resposta != resultado:
             print('Respota errada!')
             voltar = input('Deseja tentar novamente? (s/n)')
